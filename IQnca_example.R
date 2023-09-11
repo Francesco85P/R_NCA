c72fb3461edee3d21e5337ed07d42c4f20a1236b
Francesco85P <francescopaparazzo85@gmail.com>
2023-09-11 13:18:49 +0200

    k

--- IQnca_example.R
+++ IQnca_example.R
@@ -1,3 +1,6 @@
 library(tidyverse)
 library(IQnca)
-gggg
+examples_IQnca(path = "~/IntiQuan/IQnca")
+setwd("examples/03_book_examples/Example_2/")
+dataFile <- "IV_Infusion_Single_Dose.xls"
+dataOrig <- readxl::read_excel(dataFile)
